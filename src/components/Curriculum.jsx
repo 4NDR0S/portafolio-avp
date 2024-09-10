83257ab05cda00eff7514e728cbeaff9c49bf8e0
Andross <andross.vela@gmail.com>
2024-09-09 21:23:13 -0500

    correcion pdf, boton de descarga

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Curriculum({ color, py, px }) {
   return (
     <a
-      href="./cv/avp.pdf"
+      href="./avp-cv.pdf" // Ruta correcta del archivo
       download
       target="_blank"
+      rel="noopener noreferrer" // Para seguridad
     >
       <button
         className='uppercase border-2 py-1 px-8 text-[17px] hover:scale-110'
