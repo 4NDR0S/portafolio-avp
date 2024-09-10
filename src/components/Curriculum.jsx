1968cdbc956f29fae49916cef794886c9d52caa5
Andross <andross.vela@gmail.com>
2024-09-09 21:12:15 -0500

    incluir cv en el repositorio

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 export default function Curriculum({ color, py, px }) {
   return (
-    <button
-      className='uppercase border-2 py-1 px-8 text-[17px] hover:scale-110'
-      style={{
-        color: color,
-        borderColor: color,
-        paddingBlock: py,
-        paddingInline: px
-      }}
+    <a
+      href="./cv/avp.pdf"
+      download
+      target="_blank"
     >
-      descargar mi cv
-    </button>
+      <button
+        className='uppercase border-2 py-1 px-8 text-[17px] hover:scale-110'
+        style={{
+          color: color,
+          borderColor: color,
+          paddingBlock: py,
+          paddingInline: px
+        }}
+      >
+        descargar mi cv
+      </button>
+    </a>
   )
 }
